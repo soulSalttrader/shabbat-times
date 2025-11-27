6081df24b67be3e532dacf4c0aac0a4222ea2657
soulSalttrader <dusan.solarik@gmail.com>
2025-11-28 00:14:18 +0100

    feat: add RetrofitInstance using ApiUrl.BASE_SUNRISE_SUNSET and Json converter
    - Added RetrofitInstance configured with ApiUrl.BASE_SUNRISE_SUNSET.
    - Introduced JsonInstance with:
        - ignoreUnknownKeys = true
        - coerceInputValues = true
        - encodeDefaults = true
    - Added Kotlinx Serialization converter via asConverterFactory("application/json".toMediaType()).

---
+++ app/src/main/java/il/soulSalttrader/shabbat/shabbatApp/api/RetrofitInstance.kt
@@ -0,0 +1,23 @@
+package il.soulSalttrader.retro.shabbatApp.api
+
+import il.soulSalttrader.retro.playground.retrofit.data.api.ApiService
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+
+object RetrofitInstance {
+    private val JsonInstance = Json {
+        ignoreUnknownKeys = true
+        coerceInputValues = true
+        encodeDefaults = true
+    }
+
+    val api: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(ApiUrl.BASE_SUNRISE_SUNSET)
+            .addConverterFactory(JsonInstance.asConverterFactory("application/json".toMediaType()))
+            .build()
+            .create(ApiService::class.java)
+    }
+}
