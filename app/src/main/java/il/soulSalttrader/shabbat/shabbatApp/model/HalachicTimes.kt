69c067fd02df812e17c1d26af6ecaed28af8c136
soulSalttrader <dusan.solarik@gmail.com>
2025-12-02 21:12:28 +0100

    feat: add HalachicTimes data class encapsulating candleLighting and havdalah data
    - Introduced HalachicTimes to group halachic time details.
    - Includes candleLighting and havdalah (time + date).

---
+++ app/src/main/java/il/soulSalttrader/shabbat/shabbatApp/model/HalachicTimes.kt
@@ -0,0 +1,8 @@
+package il.soulSalttrader.retro.shabbatApp.model
+
+data class HalachicTimes(
+    val candleLightingTime: String = "",
+    val candleLightingDate: String = "",
+    val havdalahTime: String = "",
+    val havdalahDate: String = "",
+)
