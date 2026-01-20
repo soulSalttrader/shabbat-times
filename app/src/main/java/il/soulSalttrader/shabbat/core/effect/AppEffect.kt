90f1b577da80caaf1b9b183c95595b54d84e0e74
soulSalttrader <dusan.solarik@gmail.com>
2026-01-20 19:50:52 +0100

    feat: add OpenAppSettings to AppEffect.Shabbat

--- app/src/main/java/il/soulSalttrader/shabbat/core/effect/AppEffect.kt
+++ app/src/main/java/il/soulSalttrader/shabbat/core/effect/AppEffect.kt
@@ -14,5 +14,6 @@ sealed interface AppEffect {
     sealed interface Shabbat : AppEffect {
         data object LoadData : Shabbat
         data class LoadFailed(val error: Throwable) : Shabbat
+        data object OpenAppSettings : AppEffect
     }
 }
