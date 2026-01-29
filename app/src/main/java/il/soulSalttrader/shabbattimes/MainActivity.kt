d4dfc66bfa9d0a38b55cfff5935ae706eae88197
soulSalttrader <dusan.solarik@gmail.com>
2026-01-29 20:52:55 +0100

    chore: add MainActivity with Compose setup

---
+++ app/src/main/java/il/soulSalttrader/shabbattimes/MainActivity.kt
@@ -0,0 +1,34 @@
+package il.soulSalttrader.shabbattimes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
+import il.soulSalttrader.shabbattimes.ui.theme.ShabbatTheme
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+            ShabbatTheme {
+                Greeting("Android")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
