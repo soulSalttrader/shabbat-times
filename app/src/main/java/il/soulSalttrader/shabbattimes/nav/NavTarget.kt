b9c22fd3744fcca656bc99d6723f32a642354bee
soulSalttrader <dusan.solarik@gmail.com>
2026-01-29 19:47:15 +0100

    chore: clean up navigation by removing non-Shabbat targets

--- app/src/main/java/il/soulSalttrader/shabbattimes/nav/NavTarget.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/nav/NavTarget.kt
@@ -9,17 +9,12 @@ sealed interface NavTarget {
     companion object {
         fun NavBackStackEntry?.fromBackStackEntry(): NavTarget? {
             return when {
-                this?.destination?.hasRoute<NavTargetBottom.Alerts>() == true   -> NavTargetBottom.Alerts
-                this?.destination?.hasRoute<NavTargetBottom.Breathe>() == true  -> NavTargetBottom.Breathe
-                this?.destination?.hasRoute<NavTargetBottom.Home>() == true     -> NavTargetBottom.Home
-                this?.destination?.hasRoute<NavTargetBottom.Settings>() == true -> NavTargetBottom.Settings
-                this?.destination?.hasRoute<NavTargetBottom.Shabbat>() == true  -> NavTargetBottom.Shabbat
+                this?.destination?.hasRoute<NavTargetTop.Settings>() == true   -> NavTargetTop.Settings
+                this?.destination?.hasRoute<NavTargetTop.Previous>() == true   -> NavTargetTop.Previous
 
-                this?.destination?.hasRoute<NavTargetTop.Previous>() == true    -> NavTargetTop.Previous
-                this?.destination?.hasRoute<NavTargetTop.Favorite>() == true    -> NavTargetTop.Favorite
-                this?.destination?.hasRoute<NavTargetTop.History>() == true     -> NavTargetTop.History
-                else                                                            -> null
+                this?.destination?.hasRoute<NavTargetBottom.Shabbat>() == true -> NavTargetBottom.Shabbat
+                else                                                           -> null
             }
         }
-     }
+    }
 }
