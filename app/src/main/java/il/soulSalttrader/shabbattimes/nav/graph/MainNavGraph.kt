918958a6fee013ba91511d410df154a2742b0998
soulSalttrader <dusan.solarik@gmail.com>
2026-01-29 22:43:12 +0100

    chore: integrate Navigator, ShabbatScaffold, and NavApp into MainActivity
    - Matched the behavior of the retro repo.
    - Completed initial setup and feature extraction.

--- app/src/main/java/il/soulSalttrader/shabbattimes/nav/graph/MainNavGraph.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/nav/graph/MainNavGraph.kt
@@ -4,17 +4,11 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import il.soulSalttrader.shabbattimes.content.FailureScreen
 import il.soulSalttrader.shabbattimes.content.ShabbatScreen
-import il.soulSalttrader.shabbattimes.event.AppEvent
-import il.soulSalttrader.shabbattimes.model.State
 import il.soulSalttrader.shabbattimes.nav.NavTargetBottom
 import il.soulSalttrader.shabbattimes.nav.NavTargetTop
 import il.soulSalttrader.shabbattimes.nav.Navigator
 
-fun NavGraphBuilder.mainNavGraph(
-    state: State,
-    onEvent: (AppEvent) -> Unit,
-    navigator: Navigator,
-) {
+fun NavGraphBuilder.mainNavGraph(navigator: Navigator) {
     composable<NavTargetBottom.Shabbat> {
         ShabbatScreen()
     }
