24790a7d9211b8933d2d93259fbbe999316d6330
soulSalttrader <dusan.solarik@gmail.com>
2026-01-29 22:09:33 +0100

    feat: add FailureScreen displaying "Coming Soon" rationale in Settings screen

--- app/src/main/java/il/soulSalttrader/shabbattimes/nav/graph/MainNavGraph.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/nav/graph/MainNavGraph.kt
@@ -1,28 +1,27 @@
 package il.soulSalttrader.shabbattimes.nav.graph
 
-import androidx.compose.material3.Text
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import il.soulSalttrader.shabbattimes.content.FailureScreen
 import il.soulSalttrader.shabbattimes.content.ShabbatScreen
 import il.soulSalttrader.shabbattimes.event.AppEvent
 import il.soulSalttrader.shabbattimes.model.State
-import il.soulSalttrader.shabbattimes.nav.NavItems
 import il.soulSalttrader.shabbattimes.nav.NavTargetBottom
 import il.soulSalttrader.shabbattimes.nav.NavTargetTop
+import il.soulSalttrader.shabbattimes.nav.Navigator
 
 fun NavGraphBuilder.mainNavGraph(
     state: State,
     onEvent: (AppEvent) -> Unit,
+    navigator: Navigator,
 ) {
     composable<NavTargetBottom.Shabbat> {
         ShabbatScreen()
     }
 
     composable<NavTargetTop.Settings> {
-        NavItems.Settings.title?.let { text -> Text(text = text) }
-    }
-
-    composable<NavTargetTop.Previous> {
-        NavItems.Previous.title?.let { text -> Text(text = text) }
+        FailureScreen(message = "Coming Soon") {
+            navigator.navigateUp()
+        }
     }
 }
