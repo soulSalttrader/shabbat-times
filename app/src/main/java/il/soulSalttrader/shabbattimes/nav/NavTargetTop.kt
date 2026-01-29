4a8619252276f5578c375178fc24a42c14539e96
soulSalttrader <dusan.solarik@gmail.com>
2026-01-29 19:47:15 +0100

    chore: clean up navigation by removing non-Shabbat targets

--- app/src/main/java/il/soulSalttrader/shabbattimes/nav/NavTargetTop.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/nav/NavTargetTop.kt
@@ -5,6 +5,5 @@ import kotlinx.serialization.Serializable
 @Serializable
 sealed interface NavTargetTop : NavTarget {
     @Serializable object Previous : NavTargetTop
-    @Serializable object History : NavTargetTop
-    @Serializable object Favorite : NavTargetTop
+    @Serializable object Settings : NavTargetBottom
 }
