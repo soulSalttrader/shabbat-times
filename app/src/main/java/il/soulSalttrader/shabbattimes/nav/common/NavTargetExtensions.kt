a7570b6d5da2735dc7639013149f024bb1e59871
soulSalttrader <dusan.solarik@gmail.com>
2026-01-30 21:26:47 +0100

    feat: derive NavTarget route from qualified name

--- app/src/main/java/il/soulSalttrader/shabbattimes/nav/common/NavTargetExtensions.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/nav/common/NavTargetExtensions.kt
@@ -6,3 +6,8 @@ fun NavTarget.simpleName(): String =
     requireNotNull(this::class.simpleName) {
         "NavTarget object has no simpleName: $this. Must be top-level 'object'."
     }
+
+fun NavTarget.route(): String =
+    requireNotNull(this::class.qualifiedName) {
+        "NavTarget object has no route: $this. Must be top-level 'object'."
+    }
