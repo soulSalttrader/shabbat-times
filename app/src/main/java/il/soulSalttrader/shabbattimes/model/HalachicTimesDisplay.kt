75a355c28314168fb32ab338aee1ce3b2f1a632c
soulSalttrader <dusan.solarik@gmail.com>
2026-02-01 00:38:47 +0100

    feat: extend HalachicTimes with City context

--- app/src/main/java/il/soulSalttrader/shabbattimes/model/HalachicTimesDisplay.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/model/HalachicTimesDisplay.kt
@@ -4,6 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class HalachicTimesDisplay(
+    val city: City = Cities.JERUSALEM,
     val candleLightingTime: String = "",
     val candleLightingDate: String = "",
     val havdalahTime: String = "",
