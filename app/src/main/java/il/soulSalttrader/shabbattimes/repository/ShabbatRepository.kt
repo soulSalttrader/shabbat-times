266e8e9614e748149164aa5ed1d0298865b55555
soulSalttrader <dusan.solarik@gmail.com>
2026-02-01 00:44:10 +0100

    feat: extend ShabbatRepository.getSolarTimes with City parameter

--- app/src/main/java/il/soulSalttrader/shabbattimes/repository/ShabbatRepository.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/repository/ShabbatRepository.kt
@@ -1,11 +1,12 @@
 package il.soulSalttrader.shabbattimes.repository
 
+import il.soulSalttrader.shabbattimes.model.City
 import il.soulSalttrader.shabbattimes.model.HalachicTimesDisplay
 import il.soulSalttrader.shabbattimes.model.SolarTimes
 import il.soulSalttrader.shabbattimes.network.NetworkResult
 import java.time.LocalDate
 
 interface ShabbatRepository {
-    suspend fun getSolarTimes(date: LocalDate): NetworkResult<SolarTimes>
+    suspend fun getSolarTimes(date: LocalDate, city: City): NetworkResult<SolarTimes>
     suspend fun getHalachicTimes(): NetworkResult<HalachicTimesDisplay>
 }
