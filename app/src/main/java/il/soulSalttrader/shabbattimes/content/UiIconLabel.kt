1c99419b7c6761746bba508706fa3154154ce018
soulSalttrader <dusan.solarik@gmail.com>
2026-02-03 13:51:09 +0100

    feat: make UiIcon optional in UiIconLabel
    - Allowed UiIconLabel to render either an icon + label or just the label when no icon is provided.
    - Unified usage across composables by removing the need to choose between UiIconLabel and Text, simplifying call sites and improving reusability.
    - Replaced hardcoded textSize with MaterialTheme.typography.

--- app/src/main/java/il/soulSalttrader/shabbattimes/content/UiIconLabel.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/content/UiIconLabel.kt
@@ -2,48 +2,54 @@ package il.soulSalttrader.shabbattimes.content
 
 import androidx.compose.foundation.text.InlineTextContent
 import androidx.compose.foundation.text.appendInlineContent
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.Placeholder
 import androidx.compose.ui.text.PlaceholderVerticalAlign
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.em
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun UiIconLabel(
-    icon: UiIcon,
+    icon: UiIcon?,
     iconId: String = "currentLocationIcon",
     text: String = "Your current location",
     contentDescription: String = "Current location icon",
     iconSize: TextUnit = 1.1.em,
 ) {
-    val annotatedString = buildAnnotatedString {
-        appendInlineContent(iconId, text)
-        append(" $text")
-    }
-
-    val inlineContent = mapOf(
-        iconId to InlineTextContent(
-            placeholder = Placeholder(
-                width = iconSize,
-                height = iconSize,
-                placeholderVerticalAlign = PlaceholderVerticalAlign.Center,
-            ),
-            children = {
-                UiIconImage(
-                    icon = icon,
-                    contentDescription = contentDescription,
-                )
-            }
+    icon?.let {
+        val inlineContent = mapOf(
+            iconId to InlineTextContent(
+                placeholder = Placeholder(
+                    width = iconSize,
+                    height = iconSize,
+                    placeholderVerticalAlign = PlaceholderVerticalAlign.Center,
+                ),
+                children = {
+                    UiIconImage(
+                        icon = icon,
+                        contentDescription = contentDescription,
+                    )
+                }
+            )
         )
-    )
+
+        val annotatedString = buildAnnotatedString {
+            appendInlineContent(iconId, text)
+            append(" $text")
+        }
+
+        Text(
+            text = annotatedString,
+            inlineContent = inlineContent,
+            style = MaterialTheme.typography.bodyMedium,
+        )
+    } ?:
 
     Text(
-        text = annotatedString,
-        inlineContent = inlineContent,
-        style = TextStyle(fontSize = 16.sp)
+        text = text,
+        style = MaterialTheme.typography.bodyMedium,
     )
 }
