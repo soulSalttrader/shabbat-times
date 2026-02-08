7ead95ece3bfa7fa708339f643dacb158f793e6f
soulSalttrader <dusan.solarik@gmail.com>
2026-02-08 22:06:33 +0100

    feat: add Geoapify geocoding base URL constant

--- app/src/main/java/il/soulSalttrader/shabbattimes/network/ApiUrl.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/network/ApiUrl.kt
@@ -2,4 +2,5 @@ package il.soulSalttrader.shabbattimes.network
 
 object ApiUrl {
     const val BASE_SUNRISE_SUNSET = "https://api.sunrisesunset.io"
+    const val BASE_GEOAPIFY = "https://api.geoapify.com/v1/geocode/"
 }
