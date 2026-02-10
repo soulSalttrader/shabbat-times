63d5aec1e740991cbf414158ab20eb68cdbe2428
soulSalttrader <dusan.solarik@gmail.com>
2026-02-10 19:12:23 +0100

    feat: add GeoapifyService interface
    - Introduced GeoapifyService with three suspend functions corresponding to the available endpoints:
      - autocomplete
      - forward search
      - reverse geocode
    - Unified all endpoints to return GeoapifyResponseDto.

---
+++ app/src/main/java/il/soulSalttrader/shabbattimes/network/GeoapifyService.kt
@@ -0,0 +1,46 @@
+package il.soulSalttrader.shabbattimes.network
+
+import il.soulSalttrader.shabbattimes.BuildConfig
+import il.soulSalttrader.shabbattimes.network.dto.GeoapifyResponseDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+import java.util.Locale
+
+interface GeoapifyService {
+    @GET("autocomplete")
+    suspend fun autocomplete(
+        @Query("text") queryText: String,
+        @Query("filter") countryFilter: String? = null,
+        @Query("bias") locationBias: String? = "ipstack",
+        @Query("type") resultType: String? = "city",
+
+        @Query("limit") maxResults: Int = 5,
+        @Query("lang") preferredLanguage: String = Locale.getDefault().language,
+        @Query("format") format: String = "json",
+        @Query("apiKey") apiKey: String = BuildConfig.GEOAPIFY_API_KEY,
+    ): GeoapifyResponseDto
+
+    @GET("search")
+    suspend fun forwardSearch(
+        @Query("text") queryText: String,
+        @Query("filter") countryFilter: String? = null,
+        @Query("bias") locationBias: String? = "ipstack",
+        @Query("type") resultType: String? = "city",
+
+        @Query("limit") maxResults: Int = 5,
+        @Query("lang") preferredLanguage: String = Locale.getDefault().language,
+        @Query("format") format: String = "json",
+        @Query("apiKey") apiKey: String = BuildConfig.GEOAPIFY_API_KEY,
+    ): GeoapifyResponseDto
+
+    @GET("reverse")
+    suspend fun reverseGeocode(
+        @Query("lat") lat: Double,
+        @Query("lon") lon: Double,
+
+        @Query("limit") maxResults: Int = 1,
+        @Query("lang") preferredLanguage: String = Locale.getDefault().language,
+        @Query("format") format: String = "json",
+        @Query("apiKey") apiKey: String = BuildConfig.GEOAPIFY_API_KEY,
+    ): GeoapifyResponseDto
+}
