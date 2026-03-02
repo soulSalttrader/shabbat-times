f716b4c660d68b7d6855287b4f1ed192026758a3
soulSalttrader <dusan.solarik@gmail.com>
2026-03-02 23:35:09 +0100

    feat: add functional extensions for NetworkResult
    - Added fold extension to handle success and failure cases.
    - Added map extension to transform the data in successful results.
    - Added mapFailure extension to transform network failures.

--- app/src/main/java/il/soulSalttrader/shabbattimes/model/NetworkResultExtensions.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/model/NetworkResultExtensions.kt
@@ -7,3 +7,28 @@ fun List<NetworkResult<HalachicTimesDisplay>>.toLoadedEvent(): ShabbatDataEvent.
     filterIsInstance<NetworkResult.Success<HalachicTimesDisplay>>()
         .map { it.data }
         .let(ShabbatDataEvent.Loaded::Success)
+
+inline fun <T, R> NetworkResult<T>.fold(
+    onSuccess: (T) -> R,
+    onFailure: (NetworkResult.Failure) -> R
+): R =
+    when (this) {
+        is NetworkResult.Success -> onSuccess(data)
+        is NetworkResult.Failure -> onFailure(this)
+    }
+
+inline fun <T, R> NetworkResult<T>.map(
+    transform: (T) -> R
+): NetworkResult<R> =
+    fold(
+        onSuccess = { NetworkResult.Success(transform(it)) },
+        onFailure = { it }
+    )
+
+inline fun <T> NetworkResult<T>.mapFailure(
+    transform: (NetworkResult.Failure) -> NetworkResult.Failure
+): NetworkResult<T> =
+    when (this) {
+        is NetworkResult.Success -> this
+        is NetworkResult.Failure -> transform(this)
+    }
