369a4901502e5018083cec4ebe13a3f4605875f3
soulSalttrader <dusan.solarik@gmail.com>
2026-03-03 21:49:36 +0100

    refactor: remove unused toLoadedEvent extension
    - Removed toLoadedEvent extension function from NetworkResultExtensions.kt.

--- app/src/main/java/il/soulSalttrader/shabbattimes/model/NetworkResultExtensions.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/model/NetworkResultExtensions.kt
@@ -1,12 +1,6 @@
 package il.soulSalttrader.shabbattimes.model
 
-import il.soulSalttrader.shabbattimes.event.ShabbatDataEvent
 import il.soulSalttrader.shabbattimes.network.NetworkResult
-
-fun List<NetworkResult<HalachicTimesDisplay>>.toLoadedEvent(): ShabbatDataEvent.Loaded =
-    filterIsInstance<NetworkResult.Success<HalachicTimesDisplay>>()
-        .map { it.data }
-        .let(ShabbatDataEvent.Loaded::Success)
 
 inline fun <T, R> NetworkResult<T>.fold(
     onSuccess: (T) -> R,
