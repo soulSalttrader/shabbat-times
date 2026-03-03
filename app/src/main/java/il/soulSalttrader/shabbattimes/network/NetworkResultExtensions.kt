b837b91df722add435190fc69864a5498f2e7c90
soulSalttrader <dusan.solarik@gmail.com>
2026-03-03 23:16:12 +0100

    refactor: move NetworkResult extensions to network package
    - Relocated onSuccess, onFailure, getOrThrow, and getOrElse extensions from common to network package.
    - Updated imports in ShabbatRepositoryImpl to reflect the new location.

--- app/src/main/java/il/soulSalttrader/shabbattimes/network/NetworkResultExtensions.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/network/NetworkResultExtensions.kt
@@ -24,3 +24,25 @@ inline fun <T> NetworkResult<T>.mapFailure(
         is NetworkResult.Success -> this
         is NetworkResult.Failure -> transform(this)
     }
+
+inline fun <T> NetworkResult<T>.onSuccess(action: (T) -> Unit): NetworkResult<T> {
+    if (this is NetworkResult.Success) action(data)
+    return this
+}
+
+inline fun <T> NetworkResult<T>.onFailure(action: (NetworkResult.Failure) -> Unit): NetworkResult<T> {
+    if (this is NetworkResult.Failure) action(this)
+    return this
+}
+
+fun <T> NetworkResult<T>.getOrThrow(): T =
+    when (this) {
+        is NetworkResult.Success -> data
+        is NetworkResult.Failure -> throw RuntimeException(message, cause)
+    }
+
+inline fun <T> NetworkResult<T>.getOrElse(onFailure: (NetworkResult.Failure) -> Nothing): T =
+    when (this) {
+        is NetworkResult.Success -> data
+        is NetworkResult.Failure -> onFailure(this)
+    }
