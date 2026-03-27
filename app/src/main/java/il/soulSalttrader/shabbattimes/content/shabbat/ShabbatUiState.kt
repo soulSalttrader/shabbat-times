fb30c5725a33638eb772b830bc63431fe0e5d58f
soulSalttrader <dusan.solarik@gmail.com>
2026-03-27 23:47:59 +0100

    feat: add selectedCity to ShabbatUiState
    - Added selectedCity field to ShabbatUiState using the Selection wrapper for City objects.

--- app/src/main/java/il/soulSalttrader/shabbattimes/content/shabbat/ShabbatUiState.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/content/shabbat/ShabbatUiState.kt
@@ -1,11 +1,14 @@
 package il.soulSalttrader.shabbattimes.content.shabbat
 
+import il.soulSalttrader.shabbattimes.content.Selection
 import il.soulSalttrader.shabbattimes.location.LocationState
+import il.soulSalttrader.shabbattimes.model.City
 import il.soulSalttrader.shabbattimes.model.State
 import il.soulSalttrader.shabbattimes.permission.PermissionState
 
 data class ShabbatUiState(
     val data: ShabbatResultState = ShabbatResultState.Idle,
+    val selectedCity: Selection<City?> = Selection.Idle,
     val permission: PermissionState = PermissionState.Idle,
     val location: LocationState = LocationState.Idle,
 ) : State
