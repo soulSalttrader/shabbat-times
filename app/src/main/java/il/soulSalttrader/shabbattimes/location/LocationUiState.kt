89de2a88d63b8b1670aa003ffbc9d71738560865
soulSalttrader <dusan.solarik@gmail.com>
2026-04-15 15:47:41 +0200

    feat: add permission state to LocationUiState
    - Added permission field of type PermissionState to LocationUiState to track location permission status.

--- app/src/main/java/il/soulSalttrader/shabbattimes/location/LocationUiState.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/location/LocationUiState.kt
@@ -1,8 +1,10 @@
 package il.soulSalttrader.shabbattimes.location
 
 import il.soulSalttrader.shabbattimes.model.State
+import il.soulSalttrader.shabbattimes.permission.PermissionState
 
 data class LocationUiState(
     val location: LocationState = LocationState.Idle,
     val status: LocationStatus = LocationStatus.Unknown,
+    val permission: PermissionState = PermissionState.Idle,
 ) : State
