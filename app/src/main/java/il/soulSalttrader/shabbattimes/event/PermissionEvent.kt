c2ceab233bebdd1dc4f82fe4bce251059d794f8f
soulSalttrader <dusan.solarik@gmail.com>
2026-04-15 16:10:16 +0200

    refactor: migration of PermissionEvent to use LocationUiState and LocationReducer
    - Updated PermissionEvent to implement Reducible<LocationUiState> instead of ShabbatUiState.
    - Replaced ShabbatReducer with LocationReducer across all PermissionEvent variants.
    - Updated imports to reflect the shift from Shabbat-related components to location-related components.

--- app/src/main/java/il/soulSalttrader/shabbattimes/event/PermissionEvent.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/event/PermissionEvent.kt
@@ -1,61 +1,61 @@
 package il.soulSalttrader.shabbattimes.event
 
-import il.soulSalttrader.shabbattimes.content.shabbat.ShabbatUiState
+import il.soulSalttrader.shabbattimes.location.LocationUiState
 import il.soulSalttrader.shabbattimes.permission.PermissionState
+import il.soulSalttrader.shabbattimes.reducer.LocationReducer
 import il.soulSalttrader.shabbattimes.reducer.Reducible
-import il.soulSalttrader.shabbattimes.reducer.ShabbatReducer
 
-sealed interface PermissionEvent : AppEvent, Reducible<ShabbatUiState> {
+sealed interface PermissionEvent : AppEvent, Reducible<LocationUiState> {
     data object ShowEducation : PermissionEvent {
-        override val reducer = ShabbatReducer { state ->
+        override val reducer = LocationReducer { state ->
             state.copy(permission = PermissionState.Education)
         }
     }
 
     data object Request : PermissionEvent {
-        override val reducer = ShabbatReducer { state ->
+        override val reducer = LocationReducer { state ->
             state.copy(permission = PermissionState.Requesting)
         }
     }
 
     data object AllGranted : PermissionEvent {
-        override val reducer = ShabbatReducer { state ->
+        override val reducer = LocationReducer { state ->
             state.copy(permission = PermissionState.Granted)
         }
     }
 
     data object DeniedWithRationale : PermissionEvent {
-        override val reducer = ShabbatReducer { state ->
+        override val reducer = LocationReducer { state ->
             state.copy(permission = PermissionState.Denied)
         }
     }
 
     data object DeniedPermanently : PermissionEvent {
-        override val reducer = ShabbatReducer { state ->
+        override val reducer = LocationReducer { state ->
             state.copy(permission = PermissionState.DeniedPermanently)
         }
     }
 
     data object AcceptedRationale : PermissionEvent {
-        override val reducer = ShabbatReducer { state ->
+        override val reducer = LocationReducer { state ->
             state.copy(permission = PermissionState.Requesting)
         }
     }
 
     data object DismissedRationale : PermissionEvent {
-        override val reducer = ShabbatReducer { state ->
+        override val reducer = LocationReducer { state ->
             state.copy(permission = PermissionState.Idle)
         }
     }
 
     data object RequestedAppSettings : PermissionEvent {
-        override val reducer = ShabbatReducer { state ->
+        override val reducer = LocationReducer { state ->
             state.copy(permission = PermissionState.Idle)
         }
     }
 
     data object ReturnedFromAppSettings : PermissionEvent {
-        override val reducer = ShabbatReducer { state ->
+        override val reducer = LocationReducer { state ->
             state.copy(permission = PermissionState.Requesting)
         }
     }
