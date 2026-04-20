293fc340919debada6174252649976bd9084a0fe
soulSalttrader <dusan.solarik@gmail.com>
2026-04-20 23:33:48 +0200

    feat: add CityEvent and CityDeleted event
    - Introduced CityEvent sealed interface extending AppEvent and Reducible<CityUiState>.
    - Added CityDeleted event with a corresponding CityReducer to update the selectedCity state.

---
+++ app/src/main/java/il/soulSalttrader/shabbattimes/event/CityEvent.kt
@@ -0,0 +1,17 @@
+package il.soulSalttrader.shabbattimes.event
+
+import il.soulSalttrader.shabbattimes.content.Selection
+import il.soulSalttrader.shabbattimes.content.city.CityUiState
+import il.soulSalttrader.shabbattimes.model.City
+import il.soulSalttrader.shabbattimes.reducer.CityReducer
+import il.soulSalttrader.shabbattimes.reducer.Reducible
+
+sealed interface CityEvent : AppEvent, Reducible<CityUiState> {
+    data class CityDeleted(val city: City) : CityEvent {
+        override val reducer = CityReducer { state ->
+            state.copy(
+                selectedCity = Selection.Selected(city),
+            )
+        }
+    }
+}
