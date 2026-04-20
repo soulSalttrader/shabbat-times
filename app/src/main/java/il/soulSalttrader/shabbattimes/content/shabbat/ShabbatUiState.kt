b0bd1f2696a6e15ac22a769fa89128522320a4a6
soulSalttrader <dusan.solarik@gmail.com>
2026-04-20 23:54:32 +0200

    refactor: remove city selection from ShabbatUiState and events
    - Removed selectedCity from ShabbatUiState.
    - Removed TimeDeleted event from ShabbatDataEvent.

--- app/src/main/java/il/soulSalttrader/shabbattimes/content/shabbat/ShabbatUiState.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/content/shabbat/ShabbatUiState.kt
@@ -1,10 +1,7 @@
 package il.soulSalttrader.shabbattimes.content.shabbat
 
-import il.soulSalttrader.shabbattimes.content.Selection
-import il.soulSalttrader.shabbattimes.model.City
 import il.soulSalttrader.shabbattimes.model.State
 
 data class ShabbatUiState(
     val data: ShabbatResultState = ShabbatResultState.Idle,
-    val selectedCity: Selection<City?> = Selection.Idle,
 ) : State
