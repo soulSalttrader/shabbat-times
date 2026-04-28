4c0be0fd72ad10cba2b91b46dcea477bb8a9a4d0
soulSalttrader <dusan.solarik@gmail.com>
2026-04-28 13:22:31 +0200

    feat: add GPS location observation and resolution use cases
    - Implemented ObserveGpsLocationUseCase to provide a flow of location updates based on permission state.
    - Implemented ResolveGpsLocationUseCase to perform reverse geocoding on observed GPS coordinates.
    - Integrated PermissionRepository, GpsLocationRepository, and GeocodingRepository into the new use cases.

---
+++ app/src/main/java/il/soulSalttrader/shabbattimes/useCase/ObserveGpsLocationUseCase.kt
@@ -0,0 +1,25 @@
+package il.soulSalttrader.shabbattimes.useCase
+
+import android.location.Location
+import il.soulSalttrader.shabbattimes.location.LocationPermission
+import il.soulSalttrader.shabbattimes.repository.GpsLocationRepository
+import il.soulSalttrader.shabbattimes.repository.PermissionRepository
+import jakarta.inject.Inject
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flowOf
+
+class ObserveGpsLocationUseCase @Inject constructor(
+    private val permissionRepository: PermissionRepository,
+    private val gpsLocationRepository: GpsLocationRepository,
+) {
+    @OptIn(ExperimentalCoroutinesApi::class)
+    operator fun invoke(): Flow<Location?> = permissionRepository.permissionState
+        .flatMapLatest { permission ->
+            when (permission) {
+                is LocationPermission.Granted -> gpsLocationRepository.location
+                else                          -> flowOf(null)
+            }
+        }
+}
