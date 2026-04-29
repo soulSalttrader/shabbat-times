17d0ef7c4519f583115466d828e5520d282929bb
soulSalttrader <dusan.solarik@gmail.com>
2026-04-29 12:27:38 +0200

    refactor: enhance LocationStatus and update labeling logic

--- app/src/main/java/il/soulSalttrader/shabbattimes/location/LocationStatus.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/location/LocationStatus.kt
@@ -3,5 +3,7 @@ package il.soulSalttrader.shabbattimes.location
 sealed interface LocationStatus {
     data object Unknown : LocationStatus
     data object Current : LocationStatus
-    data class Nearby(val km: Double) : LocationStatus
+    data object NoPermission : LocationStatus
+    data object Locating : LocationStatus
+    data class Nearby(val distanceKm: Double) : LocationStatus
 }
