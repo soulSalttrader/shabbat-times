4abe88359b6a975939e61f738d08a4b77899b13d
soulSalttrader <dusan.solarik@gmail.com>
2026-04-30 14:09:46 +0200

    feat: add location status and label to LocationWithTimes

--- app/src/main/java/il/soulSalttrader/shabbattimes/model/LocationWithTimes.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/model/LocationWithTimes.kt
@@ -1,6 +1,12 @@
 package il.soulSalttrader.shabbattimes.model
 
+import il.soulSalttrader.shabbattimes.location.LocationStatus
+import il.soulSalttrader.shabbattimes.location.toLabel
+
 data class LocationWithTimes(
     val location: SavedLocation,
     val times: HalachicTimesDisplay?,
-)
+    val status: LocationStatus,
+) {
+    val label: String = status.toLabel()
+}
