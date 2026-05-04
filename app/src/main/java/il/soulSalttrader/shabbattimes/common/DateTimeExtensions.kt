839d6effdc556fa776338aea772b651664be1ca9
soulSalttrader <dusan.solarik@gmail.com>
2026-05-04 15:39:52 +0200

    refactor: remove unused date and time formatting utilities
    - Removed LocalTime.toDisplayString(context: Context) extension function.
    - Removed unused API_TIME_PARSER_12 formatter.

--- app/src/main/java/il/soulSalttrader/shabbattimes/common/DateTimeExtensions.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/common/DateTimeExtensions.kt
@@ -1,7 +1,5 @@
 package il.soulSalttrader.shabbattimes.common
 
-import android.content.Context
-import android.text.format.DateFormat
 import il.soulSalttrader.shabbattimes.constants.DateTimeFormatters.API_DATE_PARSER
 import il.soulSalttrader.shabbattimes.constants.DateTimeFormatters.API_TIME_PARSER_24
 import il.soulSalttrader.shabbattimes.constants.DateTimeFormatters.HEBREW_DATE_FORMATTER
@@ -9,8 +7,6 @@ import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.LocalDate.now
 import java.time.LocalTime
-import java.time.format.DateTimeFormatter
-import java.util.Locale
 
 fun LocalDate.nextOrTodayDayOfWeek(target: DayOfWeek): LocalDate {
     val candidate = this.with(target)
@@ -23,12 +19,6 @@ fun LocalDate.nextOrTodayDayOfWeek(target: DayOfWeek): LocalDate {
 
 fun upcomingFriday(): LocalDate = now().nextOrTodayDayOfWeek(DayOfWeek.FRIDAY)
 fun upcomingSaturday(): LocalDate = now().nextOrTodayDayOfWeek(DayOfWeek.SATURDAY)
-
-fun LocalTime.toDisplayString(context: Context): String {
-    val use24 = DateFormat.is24HourFormat(context)
-    val pattern = if (use24) "HH:mm" else "h:mm a"
-    return this.format(DateTimeFormatter.ofPattern(pattern, Locale.getDefault()))
-}
 
 fun String.toLocalDate(): LocalDate = LocalDate.parse(this, API_DATE_PARSER)
 fun String.toLocalTime(): LocalTime = LocalTime.parse(this, API_TIME_PARSER_24)
