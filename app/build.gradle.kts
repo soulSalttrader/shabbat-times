cbcf23bc81a862e4e85e01db5edca3c827815b89
soulSalttrader <dusan.solarik@gmail.com>
2026-05-25 13:05:04 +0200

    chore: bump version to v1.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = 30
         targetSdk = 36
         versionCode = 1
-        versionName = "v1.4.1"
+        versionName = "v1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
