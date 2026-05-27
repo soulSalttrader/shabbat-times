b0aa67f8de5d634de9d9a2252b05098ccd36b4a0
soulSalttrader <dusan.solarik@gmail.com>
2026-05-27 23:35:40 +0200

    test: add HiltTestRunner for dependency injection in tests
    - Introduces HiltTestRunner extending AndroidJUnitRunner to provide a HiltTestApplication during test execution.

---
+++ app/src/test/java/il/soulSalttrader/shabbattimes/HiltTestRunner.kt
@@ -0,0 +1,17 @@
+package il.soulSalttrader.shabbattimes
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+import kotlin.jvm.java
+
+class HiltTestRunner : AndroidJUnitRunner() {
+    override fun newApplication(
+        cl: ClassLoader?,
+        className: String?,
+        context: Context?
+    ): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+}
