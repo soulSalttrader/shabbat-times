1a1f78bea44bbf498cafae1cf5e5827a3770f5a1
soulSalttrader <dusan.solarik@gmail.com>
2026-05-28 16:58:40 +0200

    test: introduce TestTags for UI testing
    - Added TestTags object to centralize constants for UI testing.

---
+++ app/src/main/java/il/soulSalttrader/shabbattimes/TestTags.kt
@@ -0,0 +1,9 @@
+package il.soulSalttrader.shabbattimes
+
+object TestTags {
+    const val GPS_CARD = "gps_card"
+    const val LOCATION_CARD = "location_card"
+    const val EDUCATION_DIALOG = "education_dialog"
+    const val RATIONALE_DIALOG = "rationale_dialog"
+    const val DENIED_PERMANENTLY_DIALOG = "denied_permanently_dialog"
+}
