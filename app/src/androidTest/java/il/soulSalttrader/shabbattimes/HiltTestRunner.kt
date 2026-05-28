ff86065556be17f0afe63d6deccb2ef099b1bbef
soulSalttrader <dusan.solarik@gmail.com>
2026-05-28 09:41:13 +0200

    test: move HiltTestRunner to androidTest source set

--- app/src/androidTest/java/il/soulSalttrader/shabbattimes/HiltTestRunner.kt
+++ app/src/androidTest/java/il/soulSalttrader/shabbattimes/HiltTestRunner.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import android.content.Context
 import androidx.test.runner.AndroidJUnitRunner
 import dagger.hilt.android.testing.HiltTestApplication
-import kotlin.jvm.java
 
 class HiltTestRunner : AndroidJUnitRunner() {
     override fun newApplication(
