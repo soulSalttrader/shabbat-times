3c0bab487da70267a45c62666d71c40262a2e823
soulSalttrader <dusan.solarik@gmail.com>
2026-06-14 21:45:53 +0200

    refactor: transition from Shabbat presets to granular preferences in use cases
    - Replaced SaveShabbatPresetUseCase with SaveShabbatPreferenceUseCase to support independent updates for HavdalahCriterion and CandleLightingOffset.
    - Renamed GetShabbatPresetUseCase to ShabbatPreferencesUseCase and updated it to return a flow of ShabbatPreferences.

--- app/src/main/java/il/soulSalttrader/shabbattimes/useCase/ShabbatPreferencesUseCase.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/useCase/ShabbatPreferencesUseCase.kt
@@ -1,12 +1,12 @@
 package il.soulSalttrader.shabbattimes.useCase
 
-import il.soulSalttrader.shabbattimes.settings.ShabbatPreset
+import il.soulSalttrader.shabbattimes.settings.ShabbatPreferences
 import il.soulSalttrader.shabbattimes.settings.UserPreferences
 import jakarta.inject.Inject
 import kotlinx.coroutines.flow.Flow
 
-class GetShabbatPresetUseCase @Inject constructor(
+class ShabbatPreferencesUseCase @Inject constructor(
     private val userPreferences: UserPreferences,
 ) {
-    operator fun invoke(): Flow<ShabbatPreset> = userPreferences.shabbatPreset()
+    operator fun invoke(): Flow<ShabbatPreferences> = userPreferences.shabbatPreferences()
 }
