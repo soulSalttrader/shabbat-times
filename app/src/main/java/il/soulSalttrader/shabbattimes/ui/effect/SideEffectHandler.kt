3db6479fb1a1d50f4d89805b07076e7ca1ab4796
soulSalttrader <dusan.solarik@gmail.com>
2026-06-21 02:54:19 +0200

    refactor: make SideEffectHandler handle method suspending
    - Updated the handle function in the SideEffectHandler interface to be a suspend function to support asynchronous side effect execution.

--- app/src/main/java/il/soulSalttrader/shabbattimes/ui/effect/SideEffectHandler.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/ui/effect/SideEffectHandler.kt
@@ -3,5 +3,5 @@ package il.soulSalttrader.shabbattimes.ui.effect
 import il.soulSalttrader.shabbattimes.ui.event.UiEvent
 
 interface SideEffectHandler<E : UiEvent> {
-    fun handle(event: E, emitter: EffectEmitter)
+    suspend fun handle(event: E, emitter: EffectEmitter)
 }
