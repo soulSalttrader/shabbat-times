d31f448b13efd10b46e604f46256dd1e41bc1c33
soulSalttrader <dusan.solarik@gmail.com>
2026-06-24 00:32:23 +0200

    refactor: update location status icons in ShabbatCard

--- app/src/main/java/il/soulSalttrader/shabbattimes/ui/shabbat/ShabbatCard.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/ui/shabbat/ShabbatCard.kt
@@ -211,9 +211,9 @@ private fun UiIconLocationLabel(
             is LocationStatus.Current           -> UiIcon.Resource(R.drawable.location_on_24px)
             is LocationStatus.LastKnownLocation -> UiIcon.Resource(R.drawable.pin_history_24)
             is LocationStatus.Unknown           -> UiIcon.Resource(R.drawable.not_listed_location_24)
-            is LocationStatus.NoPermission      -> UiIcon.Resource(R.drawable.wrong_location_24)
+            is LocationStatus.NoPermission      -> UiIcon.Resource(R.drawable.location_off_24dp)
             is LocationStatus.Nearby            -> UiIcon.Resource(R.drawable.add_location24)
-            is LocationStatus.Locating          -> UiIcon.Resource(R.drawable.map_search_24dp)
+            is LocationStatus.Locating          -> UiIcon.Resource(R.drawable.moved_location_24dp)
         }
 
         UiIconLabel(
