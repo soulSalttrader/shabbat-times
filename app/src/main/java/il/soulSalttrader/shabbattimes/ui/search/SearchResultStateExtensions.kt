256519968dd70a1a0e588786b397f85a4a4acddc
soulSalttrader <dusan.solarik@gmail.com>
2026-06-24 09:54:56 +0200

    feat: integrate permission state into location status mapping

--- app/src/main/java/il/soulSalttrader/shabbattimes/ui/search/SearchResultStateExtensions.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/ui/search/SearchResultStateExtensions.kt
@@ -1,9 +1,15 @@
 package il.soulSalttrader.shabbattimes.ui.search
 
 import il.soulSalttrader.shabbattimes.model.LocationStatus
+import il.soulSalttrader.shabbattimes.permission.PermissionState
 
-fun SearchResultState.toLocationStatus() = when (this) {
-    is SearchResultState.GpsResolved -> LocationStatus.Current
-    is SearchResultState.Loading     -> LocationStatus.Locating
-    else                             -> LocationStatus.Unknown
+fun SearchResultState.toLocationStatus(permission: PermissionState) = when {
+    permission is PermissionState.Denied                     -> LocationStatus.NoPermission
+    permission is PermissionState.DeniedPermanently          -> LocationStatus.NoPermission
+    permission is PermissionState.DeniedRationale            -> LocationStatus.NoPermission
+    permission is PermissionState.DeniedPermanentlyRationale -> LocationStatus.NoPermission
+    permission is PermissionState.Requesting                 -> LocationStatus.Locating
+    this is SearchResultState.GpsResolved                    -> LocationStatus.Current
+    this is SearchResultState.Loading                        -> LocationStatus.Locating
+    else                                                     -> LocationStatus.Unknown
 }
