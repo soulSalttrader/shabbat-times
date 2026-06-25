e7bbe74b7a0f180bf24d0e859f424893d5e4f464
soulSalttrader <dusan.solarik@gmail.com>
2026-06-25 23:53:32 +0200

    feat: add location permission requested entry to OneTimeMessage
    - Added LOCATION_PERMISSION_REQUESTED to the OneTimeMessage enum to facilitate tracking the state of location permission requests.

--- app/src/main/java/il/soulSalttrader/shabbattimes/settings/OneTimeMessage.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/settings/OneTimeMessage.kt
@@ -3,4 +3,5 @@ package il.soulSalttrader.shabbattimes.settings
 enum class OneTimeMessage(val key: String) {
     UNAVAILABLE_TIME_WARNING("unavailable_time_warning"),
     LOCATION_PERMISSION_EDUCATION("location_permission_education"),
+    LOCATION_PERMISSION_REQUESTED("location_permission_requested"),
 }
