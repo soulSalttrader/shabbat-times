d165763db184d40e0e10ff3eed1d0953bbb75d44
soulSalttrader <dusan.solarik@gmail.com>
2026-06-29 23:50:42 +0200

    refactor: add UI state mapping extension for LocationPermission
    - Added the toUiState extension function to LocationPermission to simplify the conversion of domain models to PermissionUiState.
    - Utilized existing toPermissionState mapping to update the permission field within the UI state.

--- app/src/main/java/il/soulSalttrader/shabbattimes/model/LocationPermissionExtensions.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/model/LocationPermissionExtensions.kt
@@ -1,6 +1,7 @@
 package il.soulSalttrader.shabbattimes.model
 
 import il.soulSalttrader.shabbattimes.permission.PermissionState
+import il.soulSalttrader.shabbattimes.ui.permission.PermissionUiState
 
 fun LocationPermission.toPermissionState() = when (this) {
     is LocationPermission.Idle                       -> PermissionState.Idle
@@ -12,3 +13,5 @@ fun LocationPermission.toPermissionState() = when (this) {
     is LocationPermission.DeniedPermanentlyRationale -> PermissionState.DeniedPermanentlyRationale
     is LocationPermission.DeniedRationale            -> PermissionState.DeniedRationale
 }
+
+fun LocationPermission.toUiState(current: PermissionUiState) = current.copy(permission = toPermissionState())
