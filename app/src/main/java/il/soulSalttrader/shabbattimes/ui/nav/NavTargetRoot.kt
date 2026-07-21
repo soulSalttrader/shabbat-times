af49d34cae11db7039e1d2d706d0eb376c0f3d49
soulSalttrader <dusan.solarik@gmail.com>
2026-07-21 23:50:36 +0200

    refactor: rename NavTargetBottom to NavTargetRoot and update navigation roles
    - Renamed the NavTargetBottom interface and its corresponding file to NavTargetRoot.
    - Updated NavRole.BOTTOM_TAB to NavRole.ROOT to better reflect the navigation hierarchy.
    - Fixed a bug in NavTargetTop where the Settings object was incorrectly inheriting from the bottom navigation interface.
    - Updated all references to the Shabbat navigation target and start destination to use the new NavTargetRoot type.

--- app/src/main/java/il/soulSalttrader/shabbattimes/ui/nav/NavTargetRoot.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/ui/nav/NavTargetRoot.kt
@@ -3,6 +3,6 @@ package il.soulSalttrader.shabbattimes.ui.nav
 import kotlinx.serialization.Serializable
 
 @Serializable
-sealed interface NavTargetBottom : NavTarget {
-    @Serializable object Shabbat : NavTargetBottom
+sealed interface NavTargetRoot : NavTarget {
+    @Serializable object Shabbat : NavTargetRoot
 }
--- app/src/main/java/il/soulSalttrader/shabbattimes/ui/nav/graph/MainNavGraph.kt
+++ app/src/main/java/il/soulSalttrader/shabbattimes/ui/nav/graph/MainNavGraph.kt
@@ -3,12 +3,12 @@ package il.soulSalttrader.shabbattimes.ui.nav.graph
 import androidx.compose.material3.SnackbarHostState
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import il.soulSalttrader.shabbattimes.ui.nav.NavTargetBottom
+import il.soulSalttrader.shabbattimes.ui.nav.NavTargetRoot
 import il.soulSalttrader.shabbattimes.ui.nav.NavTargetTop
 import il.soulSalttrader.shabbattimes.ui.settings.SettingsScreen
 import il.soulSalttrader.shabbattimes.ui.shabbat.ShabbatScreen
 
 fun NavGraphBuilder.mainNavGraph(snackbarHostState: SnackbarHostState) {
-    composable<NavTargetBottom.Shabbat> { ShabbatScreen(snackbarHostState) }
+    composable<NavTargetRoot.Shabbat> { ShabbatScreen(snackbarHostState) }
     composable<NavTargetTop.Settings> { SettingsScreen() }
 }
